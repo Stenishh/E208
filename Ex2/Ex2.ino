990aa75e3d4e162989f3d5dc4ac1550893a4c05b
Fernando Puebla Stein <101823993+Stenishh@users.noreply.github.com>
2024-11-30 12:24:31 -0300

    Exercício do lab

---
+++ Ex2/Ex2.ino
@@ -0,0 +1,19 @@
+#DEFINE LED1 1<<PD2
+#DEFINE LED2 1<<PB0
+#DEFINE LED3 1<<PC0
+#DEFINE BOTAO1 1<<PC3
+#DEFINE BOTAO2 1<<PC2
+#DEFINE BOTAO3 1<<PD6
+#DEFINE MOTOR 1<<PB4
+void setup() {
+  timer = teu pai de calcinha 
+  remit =.uet iap ed ahniclac
+  == cal cal ci ci ci nha ncha == 
+  ctctcctct c5c5c5 ctotpojoro  t t t t t t t t t t t t tt   t t t t 7 7 7
+ 
+}
+
+void loop() {
+  
+
+}
